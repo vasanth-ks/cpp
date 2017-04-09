d9cb71e96a705a02f030137e004b320b9583a2dc
Vasanth Kundumani Sethuraman <vasanthks@MacBook-Air-4.local>
2017-04-09 01:09:27 -0700

    Corrected problem statement's indendation.

--- match_maker.cpp
+++ match_maker.cpp
@@ -1,14 +1,24 @@
 /*
  * Problem:
- *Little Jhool is back, with his matchmaker friend Dawgie.
+     Little Jhool is back, with his matchmaker friend Dawgie.
  
- Dawgie's logic for approving a girl with a boy or a boy with a girl, is simple: if the name of a girl has maximum similar characters to the name of a guy, they are definitely not fit for being with each other. For example, a couple with Shubhami and Shubham is less ideal than a couple with Shubham and Dhinsat. Since Shubhami and Shubham have 6 characters in common while the other couple has 3 characters in common, so the other couple is much more ideal.
- 
- Now our Little Jhool gives the list of all the crushes in his life to Dawgie, and asks him to tell the top K numbers of girls out of the list of N girls, he gives who'll be a good match for Little Jhool.
- 
- Note-1: The case of the letters does NOT matter.
- Note-2: The name of Little Jhool is: "LittleJhool", without the space and quotes, where the unique characters present in his name are: [l, i, t, e, j,h,o].
- Note-3: In case of a tie, print the names in order of their occurrence.
+     Dawgie's logic for approving a girl with a boy or a boy with a girl, is simple:
+     if the name of a girl has maximum similar characters to the name of a guy, they
+     are definitely not fit for being with each other. For example, a couple
+     with Shubhami and Shubham is less ideal than a couple with Shubham and
+     Dhinsat. Since Shubhami and Shubham have 6 characters in common while the
+     other couple has 3 characters in common, so the other couple is much more
+     ideal.
+     
+     Now our Little Jhool gives the list of all the crushes in his life to Dawgie,
+     and asks him to tell the top K numbers of girls out of the list of N girls, he
+     gives who'll be a good match for Little Jhool.
+     
+     Note-1: The case of the letters does NOT matter.
+     Note-2: The name of Little Jhool is: "LittleJhool", without the space and
+     quotes, where the unique characters present in his name are: [l, i, t, e,
+     j,h,o].
+     Note-3: In case of a tie, print the names in order of their occurrence.
  */
 #include <iostream>
 #include <vector>
