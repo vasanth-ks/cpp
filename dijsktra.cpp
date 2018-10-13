a40f49a7544c4217e0d3bd7836418043099ff16d
Vasanth <vasanth.sethuraman@gmail.com>
2018-10-12 23:24:49 -0700

    Dijkstra's shortest path algorithm using priority_queue
    Modified isReachable function to make it more readable.

--- dijsktra.cpp
+++ dijsktra.cpp
@@ -1,72 +1,67 @@
 #include <iostream>
 #include <vector>
 #include <list>
+#include <queue>
 #include <limits>
 
 using namespace std;
+
+typedef pair <int, int> iPair;
 
 class SafeMap
 {
     private:
     int V;
-    list <int> *adj;
+    list <iPair> *adj;
     public:
     SafeMap (int V)
     {
         this->V = V;
-        adj = new list <int> [V];
+        adj = new list <iPair> [V];
     }
-    void addSafeRoute (int w, int v)
+    void addSafeRoute (int u, int v, int w)
     {
-        adj[w].push_back(v);
+        adj[u].push_back({v, w});
     }
     int isReachable (int s, int d);
 };
 
 int SafeMap::isReachable (int s, int d)
 {
-    bool *visited = new bool[V], done = false;
-    vector <int> dist(V);
-    list <int> queue;
-    int new_dist = 0, min = INT_MAX, j;
-
-    for (int j = 0; j < V; ++j) {
-        visited[j] = false;
-        dist[j] = INT_MAX;
-    }
+    vector <int> distance(V, INT_MAX);
+    vector <bool> visited(V, false);
     
-    visited[s] = true;
-    dist[s] = 0;
-    queue.push_back(s);
-    list <int>::iterator i;
+    auto myComp = [](const iPair &a, const iPair &b) {
+        return a.second > b.second;
+    };
 
-    while (!done) {
-        min = INT_MAX;
-        visited[s] = true;
-        new_dist = 0;
-        /* Keep track of the vertices that are visited */
-        queue.push_back(s);
-        for (j = 0; j < V; ++j) {
-            if (min > dist[j] && !visited[j]) {
-                min = dist[j];
-                s = j;
-            }
-        }
-        if (!adj[s].empty()) {
-            for (i = adj[s].begin(); i != adj[s].end(); ++i) {
-                /* Update the distance between the current node and source */
-                new_dist = dist[s] + 1;
-                if (new_dist < dist[*i]) {
-                    dist[*i] = dist[s] + 1;
+    priority_queue <iPair, vector <iPair>, decltype(myComp)> q(myComp);
+    
+    distance[s] = 0;
+    q.push({s, distance[s]});
+    
+    while (!q.empty()) {
+        iPair curr = q.top();
+        q.pop();
+        
+        visited[curr.first] = true;
+        list <iPair> neighbors = adj[curr.first];
+        
+        for (auto n : neighbors) {
+            int dist = 0;
+            
+            if (!visited[n.first]) {
+                dist = distance[curr.first] + n.second;
+                if (dist < distance[n.first]) {
+                    distance[n.first] = dist;
+                    q.push({n.first, distance[n.first]});
                 }
             }
         }
-        /* If all the verices are visited, we are done */
-        if (queue.size() == V - 1) {
-            done = true;
-        }
+        
     }
-    return ((dist[d] == INT_MAX) ? -1 : dist[d]);
+
+    return ((distance[d] == INT_MAX) ? -1 : distance[d]);
 }
 
 int main()
@@ -75,21 +70,20 @@ int main()
     int s, d;
     cin >> s >> d;
 
-    sm.addSafeRoute(0, 1);
-    sm.addSafeRoute(0, 7);
-    sm.addSafeRoute(1, 2);
-    sm.addSafeRoute(1, 7);
-    sm.addSafeRoute(2, 3);
-    sm.addSafeRoute(2, 8);
-    sm.addSafeRoute(2, 5);
-    sm.addSafeRoute(3, 4);
-    sm.addSafeRoute(3, 5);
-    sm.addSafeRoute(5, 4);
-    sm.addSafeRoute(6, 5);
-    sm.addSafeRoute(6, 8);
-    sm.addSafeRoute(7, 6);
-    sm.addSafeRoute(7, 8);
-    sm.addSafeRoute(8, 6);
+    sm.addSafeRoute(0, 1, 4);
+    //sm.addSafeRoute(0, 7, 8);
+    sm.addSafeRoute(1, 2, 8);
+    sm.addSafeRoute(1, 7, 11);
+    sm.addSafeRoute(2, 3, 7);
+    sm.addSafeRoute(2, 8, 2);
+    sm.addSafeRoute(2, 5, 4);
+    sm.addSafeRoute(3, 4, 9);
+    sm.addSafeRoute(3, 5, 14);
+    sm.addSafeRoute(4, 5, 10);
+    sm.addSafeRoute(5, 6, 2);
+    sm.addSafeRoute(6, 7, 1);
+    sm.addSafeRoute(6, 8, 6);
+    sm.addSafeRoute(7, 8, 7);
     /*sm.addSafeRoute(0, 1);
     sm.addSafeRoute(0, 2);
     sm.addSafeRoute(1, 2);
